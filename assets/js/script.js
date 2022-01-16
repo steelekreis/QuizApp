c5585f0fe4f69b67aa91cb646440792124b67661
steelekreis <steele.kreis@gmail.com>
2022-01-15 20:10:57 -0700

    added last of the functions, now testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,43 +9,45 @@ var beginButton = document.getElementById('begin');
 var questionText = document.getElementById('qtext');
 var questionResult = document.getElementById('result');
 var finalScore = document.getElementById('finalScore');
-var userScore = document.getElementById('score');
+var userfinalScore = document.getElementById('score');
 var highscoreSection = document.getElementById('highScores');
 var highscoreList = document.getElementById('scoreList');
 var tryAgain = document.getElementById('tryAgain');
 var clearScores = document.getElementById('clear');
+var submitScore = document.getElementById('submit');
+var initials = document.getElementById('initials');
 
 
 const questions = [
   {
     question: "What is Harry Potter's middle name?",
-    answers: {
-      a: "John",
-      b: "James",
-      c: "Sirius",
-      d: "Jane"
-    },
-    correctAnswer: "b"
+    answers: [
+      "John",
+      "James",
+      "Sirius",
+      "Jane"
+    ],
+    correctAnswer: "James"
   },
   {
     question: "How does one get into the Hogwarts kitchens?",
-    answers: {
-      a: "Tickle a painting of a pear",
-      b: "Tap the bottom middle barrel 7 times",
-      c: "Give Dobby a sock!",
-      d: "Walk back and forth down the corridor three times while hungry"
-    },
-    correctAnswer: "a"
+    answers: [
+      "Tickle a painting of a pear",
+      "Tap the bottom middle barrel 7 times",
+      "Give Dobby a sock!",
+      "Walk back and forth down the corridor three times while hungry"
+    ],
+    correctAnswer: "Tickle a painting of a pear"
   },
   {
     question: "In what year did Dumbledore defeat the dark wizard Grindelwald?",
-    answers: {
-      a: "1939",
-      b: "1947",
-      c: "1919",
-      d: "1945"
-    },
-    correctAnswer: "d"
+    answers: [
+      "1939",
+      "1947",
+      "1919",
+      "1945"
+    ],
+    correctAnswer: "1945"
   }
 ];
 
@@ -54,7 +56,7 @@ const questions = [
 
 var highScores = function() {
   var storedScores = JSON.parse(localStorage.getItem('storeScore')) || [];
-  for (var index = 0; i < storedScores.length, index++) {
+  for (var index = 0; index < storedScores.length; index++) {
     var scoreItem = document.createElement('li');
     scoreItem.textContent = storedScores[index].score + " " + storedScores[index].name;
     highscoreList.appendChild(scoreItem);
@@ -70,7 +72,7 @@ clearScores.addEventListener('click', function() {
 })
 
 //Try again button and function
-
+tryAgain.addEventListener ("click", quiz);
 // GIVEN I am taking a code quiz
 
 
@@ -78,33 +80,110 @@ clearScores.addEventListener('click', function() {
 // WHEN I click the start button
 startButton.addEventListener ("click", quiz);
 // THEN a timer starts and I am presented with a question
-function quiz() {
-  document.getElementById('landing').style.display = 'none';
-  document.getElementById('landing').style.display = 'block';
-  var timeLeft = 75;
-  var timeInterval = setInterval(function () {
-    if (timeLeft > 0) {
-      time.textContent =`Time: ${timeLeft}`;
-      timeLeft--;
-    } else {
-      time.textContent = '';
-      clearInterval(timeInterval);
-      submitReveal();
-    }
-  }, 1000);
-  var output = [];
-  var answers;
-  for(var i=0; i<questions.length; i++){
-    answers = [];
-    
-  }
-}
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
+var timeLeft = 20;
+var timer;
+var questionIndex = 0;
+var userScore = 90;
 
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+var random = function (min, max) {
+  var rando = Math.floor(Math.random() * (max-min) + min);
+  return rando;
+};
+
+function quiz() {
+  document.getElementById('intro').style.display = 'none';
+  document.getElementById('questions').style.display = 'block';
+
+  timer = setInterval(clockTick, 1000);
+  generateQuestion();
+};
+
+
+
+function generateQuestion() {
+  var currentQuestion = questions[questionIndex];
+  var lastQuestion = questions[questions.length + 1];
+
+  if (currentQuestion == lastQuestion) {
+    endQuiz();
+  }
+  else {
+    questionText.textContent = currentQuestion.question;
+    questionText.setAttribute('class','question-title');
+    questionResult.innerHTML = '';
+    currentQuestion.answers.forEach(function(element) {
+      var qStuff = document.createElement('button');
+      qStuff.textcontent = element;
+      qStuff.setAttribute('class','cta cta-primary quiz-button');
+      qStuff.setAttribute('value', element);
+      qStuff.onclick = validate;
+      questionResult.appendChild(qStuff);
+    });
+  }
+};
+
+function validate() {
+  var currentQuestion = questions[questionIndex];
+  var userAnswer = this.value;
+
+  if (userAnswer === currentQuestion.correctAnswer) {
+    questionResult.textContent = 'Correct';
+    questionResult.setAttribute('class','question-results');
+  }
+  else {
+    questionResult.textContent = 'Incorrect';
+    questionResult.setAttribute('class','question-results');
+    timeLeft -= 5;
+    clockTick;
+  };
+
+  questionIndex++;
+
+  generateQuestion;
+};
+
+function clockTick() {
+  timeLeft--
+
+  countdown.textContent = timeLeft;
+  if (timeLeft <= 0) {
+    endQuiz;
+  };
+};
+
+function endQuiz() {
+  clearInterval(timer);
+  qSection.style.display = 'none';
+  finalScore.style.display = 'block';
+  score.textContent = timeLeft;
+};
+
+submitScore.addEventListener ('click', saveScore);
+
+function saveScore () {
+  if (initials.value === "") {
+    alert('Please enter you initials in order to submit!');
+    return false;
+  }
+  else {
+    var highScores = JSON.parse(localStorage.getItem('storeScore')) || [];
+    var initialValue = initials.value.trim();
+    var storeScore = {
+      name: initialValue,
+      score: timeLeft
+    };
+  }
+
+  initials.value = "";
+  highScores.push(storeScore);
+  localStorage.setItem("storeScore", JSON.stringify(highScores));
+};
+
+var loadData = function() {
+  var storedScores = JSON.parse(localStorage.getItem('storeScore')) || [];
+  for (var index = 0; index < storedScores.length; index++) {
+    var scoreList = document.createElement('li');
+    scoreList.textContent = storedScores[index].name + " " + storedScores[index].score;
+    highscoreList.appendChild(scoreList);
+  }
+};
